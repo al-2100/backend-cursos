c7b0913f86337346f8e98e10ee973db1413816b2
Jorge Barriga <alonsoxbarriga@hotmail.com>
2025-04-05 17:25:02 -0500

    Configurar conexión con PostgreSQL

--- cursos_project/settings.py
+++ cursos_project/settings.py
@@ -84,10 +84,15 @@ WSGI_APPLICATION = 'cursos_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cursos',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
